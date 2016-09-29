0da7dc0845129347e8f46817bb96d256e666ca04
Patricia Suriana <patricia.suriana@gmail.com>
2016-09-29 12:04:34 -0700

    Add method to find fused group

--- src/RealizationOrder.cpp
+++ src/RealizationOrder.cpp
@@ -37,29 +37,50 @@ void realization_order_dfs(string current,
     order.push_back(current);
 }
 
-void find_fused_group(string current,
-                      const map<string, vector<FusedPair>> &fused_pairs_graph,
-                      set<string> &visited,
-                      vector<vector<string>> &group) {
+void find_fuse_group_dfs(string current,
+                         const map<string, set<string>> &fuse_adjacency_list,
+                         set<string> &visited,
+                         vector<string> &group) {
     visited.insert(current);
+    group.push_back(current);
 
-    map<string, vector<FusedPair>>::const_iterator iter = fused_pairs_graph.find(current);
-    internal_assert(iter != fused_pairs_graph.end());
+    map<string, set<string>>::const_iterator iter = fuse_adjacency_list.find(current);
+    internal_assert(iter != fuse_adjacency_list.end()) << "current: " << current << "\n";
 
     for (const string &fn : iter->second) {
         if (visited.find(fn) == visited.end()) {
-            find_fused_group(fn, fused_pairs_graph, visited, group);
+            find_fuse_group_dfs(fn, fuse_adjacency_list, visited, group);
         }
     }
-
-    order.push_back(current);
 }
 
-void collect_fused_pairs(const map<string, map<string, Function>> &indirect_calls,
+vector<vector<string>> find_fuse_group(const map<string, Function> &env,
+                                       const map<string, set<string>> &fuse_adjacency_list) {
+    set<string> visited;
+    vector<vector<string>> result;
+
+    for (const auto &iter : env) {
+        if (visited.find(iter.first) == visited.end()) {
+            vector<string> group;
+            find_fuse_group_dfs(iter.first, fuse_adjacency_list, visited, group);
+            result.push_back(std::move(group));
+        }
+    }
+    return result;
+}
+
+void collect_fused_pairs(const map<string, Function> &env,
+                         const map<string, map<string, Function>> &indirect_calls,
                          const vector<FusedPair> &pairs,
                          vector<FusedPair> &func_fused_pairs,
-                         map<string, set<string>> &graph) {
+                         map<string, set<string>> &graph,
+                         map<string, set<string>> &fuse_adjacency_list) {
     for (const auto &p : pairs) {
+        if (env.find(p.func_2) == env.end()) {
+            // Since func_2 is not being called by anyone, might as well skip this fused pair.
+            continue;
+        }
+
         // Assert no duplicates (this technically should not have been possible from the front-end)
         internal_assert(std::find(func_fused_pairs.begin(), func_fused_pairs.end(), p) == func_fused_pairs.end())
              << "Found duplicates of fused pair (" << p.func_1 << ".s" << p.stage_1 << ", "
@@ -82,6 +103,9 @@ void collect_fused_pairs(const map<string, map<string, Function>> &indirect_call
             }
         }
 
+        fuse_adjacency_list[p.func_1].insert(p.func_2);
+        fuse_adjacency_list[p.func_2].insert(p.func_1);
+
         func_fused_pairs.push_back(p);
         graph[p.func_2].insert(p.func_1);
     }
@@ -97,6 +121,7 @@ vector<string> realization_order(const vector<Function> &outputs,
         indirect_calls.emplace(caller.first, std::move(more_funcs));
     }
 
+    debug(0) << "\n";
     for (const auto &it1 : indirect_calls) {
         debug(0) << "Function calls by: " <<  it1.first << "\n";
         for (const auto &it2 : it1.second) {
@@ -112,6 +137,8 @@ vector<string> realization_order(const vector<Function> &outputs,
     // Each function maps to the list of Functions computed_with it.
     map<string, vector<FusedPair>> fused_pairs_graph;
 
+    map<string, set<string>> fuse_adjacency_list;
+
     for (const pair<string, Function> &caller : env) {
         set<string> &s = graph[caller.first];
         for (const pair<string, Function> &callee : find_direct_calls(caller.second)) {
@@ -121,13 +148,16 @@ vector<string> realization_order(const vector<Function> &outputs,
         // Find all compute_with (fused) pairs. We have to look at the update
         // definitions as well since compute_with is defined per definition.
         vector<FusedPair> &func_fused_pairs = fused_pairs_graph[caller.first];
-        collect_fused_pairs(
-            indirect_calls, caller.second.definition().schedule().fused_pairs(), func_fused_pairs, graph);
+        fuse_adjacency_list[caller.first]; // Make sure every Func in 'env' is allocated a slot
+        collect_fused_pairs(env, indirect_calls, caller.second.definition().schedule().fused_pairs(),
+            func_fused_pairs, graph, fuse_adjacency_list);
         for (const Definition &def : caller.second.updates()) {
-            collect_fused_pairs(indirect_calls, def.schedule().fused_pairs(), func_fused_pairs, graph);
+            collect_fused_pairs(env, indirect_calls, def.schedule().fused_pairs(),
+                func_fused_pairs, graph, fuse_adjacency_list);
         }
     }
 
+    debug(0) << "\n";
     for (const auto &i : fused_pairs_graph) {
         debug(0) << "Fused pairs of Func " << i.first << "\n";
         for (const auto &iter : i.second) {
@@ -136,10 +166,20 @@ vector<string> realization_order(const vector<Function> &outputs,
         }
     }
 
+    debug(0) << "\n";
     for (const auto &i : graph) {
         debug(0) << "Callees of Func " << i.first << "\n";
         for (const auto &callee : i.second) {
             debug(0) << callee << ", ";
+        }
+        debug(0) << "\n";
+    }
+
+    debug(0) << "\n";
+    for (const auto &i : fuse_adjacency_list) {
+        debug(0) << "Neighbors of Func " << i.first << "\n";
+        for (const auto &fn : i.second) {
+            debug(0) << fn << ", ";
         }
         debug(0) << "\n";
     }
@@ -180,6 +220,17 @@ vector<string> realization_order(const vector<Function> &outputs,
         debug(0) << iter << ", ";
     }
     debug(0) << "\n";
+
+    vector<vector<string>> fuse_group = find_fuse_group(env, fuse_adjacency_list);
+
+    debug(0) << "\n";
+    for (const auto &group : fuse_group) {
+        debug(0) << "Fused group: " << "\n";
+        for (const auto &fn : group) {
+            debug(0) << fn << ", ";
+        }
+        debug(0) << "\n";
+    }
 
     return order;
 }
